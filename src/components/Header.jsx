9e74904e15ca63ea88facb20209f96540e8e0764
Gaßmann, Aaron <Aaron.Gassmann@dts.de>
2025-10-09 15:44:08 +0200

    site icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
+import icon from "../../public/webicon.png";
+
 function Header() {
   return (
     <>
       <header className="lexicon-header">
         <div className="header-content">
-          <h1 className="site-title">Götzikon</h1>
+          {/* New container to group the icon and title */}
+          <div className="site-title-container">
+            <img src={icon} alt="Götzikon website icon" className="site-icon" />
+            <h1 className="site-title">Götzikon</h1>
+          </div>
 
           <div className="donation-section">
             <p className="donation-info">
